f03d267d7a7296d722c4d8f0dc949d2a6577df2e
Josueledo <116981940+Josueledo@users.noreply.github.com>
2025-02-27 18:18:22 -0300

    feat- api emailjs

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -4,11 +4,12 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
 import { FooterComponent } from '../footer/footer.component';
 import { ContactButtonComponent } from "../contact-button/contact-button.component";
+import { EmailComponent } from "../email/email.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, FontAwesomeModule, FooterComponent, ContactButtonComponent],
+  imports: [HeaderComponent, FontAwesomeModule, FooterComponent, ContactButtonComponent, EmailComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
